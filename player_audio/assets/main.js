3638ebaa832a1dce750c00cf7d69d30a6bc50d66
Ammar_Shihan <mmarsh80@hotmail.com>
2022-12-20 16:58:06 +0100

    barre d'avancement avec time fonctionel !

--- player_audio/assets/main.js
+++ player_audio/assets/main.js
@@ -34,40 +34,61 @@
 
 // let play = document.querySelector(".btn_play");
 let playStatus = false;
-let audio = new Audio("./assets/img/Vianney-Kendji-Girac-Le-feu.mp3");
+// let audio = new Audio("./assets/img/Vianney-Kendji-Girac-Le-feu.mp3");
+let audio = new Audio("./assets/img/Nettson-Mood.mp3");
 let play_stop = document.querySelector("#play_stop");
+let countMyliste = 0;
+
+
+
 play_stop.addEventListener("click", (e) => {
     playStop();
-});
 
+});
+/**
+ * démarrer/arrêter la musique
+ */
 function playStop() {
-    if (playStatus === false) {
+    if ((playStatus === false) & (countMyliste % 2 == 1)) {
+        document.querySelector(".custom-slider-son").value = 25;
         play_stop.className = "pause"
         audio.play();
         playStatus = true;
+        soundIcon();
 
-    } else {
+    } else if (playStatus === true) {
         play_stop.className = "play"
         audio.pause();
         playStatus = false;
     }
 };
 
+/**
+ * Donner une classe de style à une balise (Avec Toggle 0/1)
+ * @param {*} selecteur nom de la .class/#id (attention aux . & #) de la balise cible
+ * @param {*} styl classe de style voulu 
+ */
 function list_recharge(selecteur, styl) {
+    countMyliste++;
     document.querySelector(selecteur).classList.toggle(styl);
 };
-
+/**
+ * 
+ * Donner une classe de style à une balise
+ * @param {*} selecteur nom de la .class/#id (attention aux . & #) de la balise cible 
+ * @param {*} sty classe de style voulu 
+ */
 function sound_charge(selecteur, sty) {
     document.querySelector(selecteur).classList.add(sty);
 };
-
-
+/**
+ * Enlever une classe de style 
+ * @param {*} selecteur nom de la .class/#id (attention aux . & #) de la balise cible 
+ * @param {*} sty classe de style voulu
+ */
 function sound_remove(selecteur, sty) {
     document.querySelector(selecteur).classList.remove(sty);
 };
-
-
-sound_charge(".sound_off","C_sound_show");
 
 let recharg = document.querySelector("#library-link");
 recharg.addEventListener("click", (e) => {
@@ -75,52 +96,78 @@ recharg.addEventListener("click", (e) => {
 }
 );
 
-
-
-
-
-// console.log(volumeValue);
-function soundIcon(){
-    const soundValue = document.querySelector(".sound-control input").value;
-    if (soundValue==0){
-        sound_charge(".sound_off","C_sound_show");
-        sound_remove(".sound_low","C_sound_show");
-        sound_remove(".sound_hight","C_sound_show");
-        sound_remove(".sound_off","C_sound_hidden_sound_off");
-    }
-    else if((soundValue >0) & (soundValue <=50)){
-        sound_charge(".sound_off","C_sound_hidden_sound_off");
-        sound_remove(".sound_off","C_sound_show");
-        sound_charge(".sound_low","C_sound_show");
-        sound_remove(".sound_hight","C_sound_show");
-
-    }
-    else if(soundValue >50){
-        sound_remove(".sound_off","C_sound_show");
-        sound_remove(".sound_low","C_sound_show");
-        sound_charge(".sound_hight","C_sound_show");
-    }
-}
-
+//* sound control
+//* sound =0 onload
+document.addEventListener("DOMContentLoaded", (e) => {
+    document.querySelector(".custom-slider-son").value = 0;
+});
 //* sound volume control
 const volume = document.querySelector(".sound-control input");
-volume.addEventListener("change", () => {
-    let volumeValue = document.querySelector(".sound-control input").value;
-console.log(volumeValue);
+volume.addEventListener("change", (e) => {
     audio.volume = volume.value / 100;
     soundIcon();
 });
+
+//* sound icon control
+function soundIcon() {
+    const soundValue = document.querySelector(".sound-control input").value;
+    if (soundValue == 0) {
+        sound_charge(".sound_off", "C_sound_show");
+        sound_remove(".sound_low", "C_sound_show");
+        sound_remove(".sound_hight", "C_sound_show");
+        sound_remove(".sound_off", "C_sound_hidden_sound_off");
+    }
+    else if ((soundValue > 0) & (soundValue <= 50)) {
+        sound_charge(".sound_off", "C_sound_hidden_sound_off");
+        sound_remove(".sound_off", "C_sound_show");
+        sound_charge(".sound_low", "C_sound_show");
+        sound_remove(".sound_hight", "C_sound_show");
+
+    }
+    else if (soundValue > 50) {
+        sound_remove(".sound_off", "C_sound_show");
+        sound_remove(".sound_low", "C_sound_show");
+        sound_charge(".sound_hight", "C_sound_show");
+        return soundValue;
+    }
+}
+
+
+
+
+
+
+
+
+
+
 //*defining audio and song info
 //format current/duration time
 function timeFormat(time) {
-  return Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2);
+    return Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2);
 }
 
 
 const durationInput = document.querySelector(".player input");
-const currentTime = document.querySelector(".player span");
+const currentTime = document.querySelector(".time_avance p");
 
+audio.addEventListener("loadedmetadata", () => {
+    const endTime = document.querySelector(".time_avance p:last-child");
+    durationInput.value = audio.currentTime;
+    durationInput.setAttribute("max", audio.duration);
+    currentTime.innerText = `${timeFormat(audio.currentTime)}`;
+    endTime.innerText = `${timeFormat(audio.duration)}`;
+});
 
+audio.addEventListener("timeupdate", () => {
+    durationInput.value = audio.currentTime;
+    currentTime.innerText = `${timeFormat(audio.currentTime)}`;
+    document.querySelector(".time_avance p:last-child").style.left = `${(audio.currentTime / audio.duration) * 100}%`;
+});
+
+durationInput.addEventListener("change", () => {
+    audio.currentTime = durationInput.value;
+});
 // const libraryLink = document.getElementById("library-link");
 // libraryLink.addEventListener("click", openLibrary);
 // const library = document.querySelector(".library");
